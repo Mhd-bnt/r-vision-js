653221dfaf9e413014c2c397596f0c9001295c92
Mehdi <mehdi@example.com>
2025-06-27 19:02:44 +0200

    Practice Dom manipulation : Working with classes

--- modal/script.js
+++ modal/script.js
@@ -1,7 +1,26 @@
 'use strict';
 
-const btn = document.querySelector('.show-modal');
 const modal = document.querySelector('.modal');
-const showModal = btn.addEventListener('click', () => {
-  modal.classList('show-modal');
-});
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+// limitation of querySelector() with show-modal we have 3 buttons but only the first one is targeted thats how querySelector works only the first element will be targeted even if multiple elements have the same className, so we can use querySelectorAll
+// const showModal = btn.addEventListener('click', () => {
+//   modal.classList('show-modal');
+// });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden'); // we can remove multiple classes by separiting them by , , (!!! when we remove classname we don't need to add a dot !!!)
+};
+
+for (const btn of btnOpenModal) {
+  btn.addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal); // No parenthesis  here !!! js would call the function immediatly if we do that !!!!
+
+overlay.addEventListener('click', closeModal);
