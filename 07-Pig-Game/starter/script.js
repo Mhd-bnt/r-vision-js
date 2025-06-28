c1c9e519d4f89df8bb4aabb9166bbbab320ed337
Mehdi <mehdi@example.com>
2025-06-28 14:19:51 +0200

    Practice Dom manipulation : Holding current score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,13 @@
 'use strict';
 // Selecting elements :
 // ---------------------
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
+let score0El = document.querySelector('#score--0');
+let score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 
 // Selecting button elements :
 // ---------------------------
@@ -21,6 +23,7 @@ diceEl.classList.add('hidden');
 //Player current score :
 let currentScore = 0;
 
+// ------------------------------
 // Rolling dice Functionnality :
 // ------------------------------
 btnRoll.addEventListener('click', () => {
@@ -35,8 +38,46 @@ btnRoll.addEventListener('click', () => {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    player0.classList.contains('player--active')
+      ? (current0El.textContent = currentScore)
+      : (current1El.textContent = currentScore);
   } else {
     //Switch to next player
+    if (player0.classList.contains('player--active')) {
+      current0El.textContent = 0;
+      currentScore = 0;
+      player0.classList.remove('player--active');
+      player1.classList.add('player--active');
+    } else {
+      current1El.textContent = 0;
+      currentScore = 0;
+      player0.classList.add('player--active');
+      player1.classList.remove('player--active');
+    }
+  }
+});
+
+// ------------
+// Hold button
+// ------------
+let count0 = 0;
+let count1 = 0;
+btnHold.addEventListener('click', () => {
+  if (player0.classList.contains('player--active')) {
+    count0 += currentScore;
+    score0El.textContent = count0;
+    current0El.textContent = 0;
+
+    currentScore = 0;
+    player0.classList.remove('player--active');
+    player1.classList.add('player--active');
+  } else {
+    count1 += currentScore;
+    score1El.textContent = count1;
+    current1El.textContent = 0;
+
+    currentScore = 0;
+    player1.classList.remove('player--active');
+    player0.classList.add('player--active');
   }
 });
