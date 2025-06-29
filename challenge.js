891fca4ac1ac21ab35fcf88194ff6530bf311443
Mehdi <mehdi@example.com>
2025-06-29 20:53:46 +0200

    Challenge #4

--- challenge.js
+++ challenge.js
@@ -84,3 +84,64 @@ for (const [team, chance] of odd) {
 //        Lewandowski: 2
 //      }
 // GOOD LUCK😀
+
+// -------------
+console.log("--------Challenge #4 --------");
+// Challenge 4 :
+// -------------
+// Write a program that receives a list of variable names written in underscore_case
+// and convert them to camelCase.
+// The input will come from a textarea inserted into the DOM (see code below to
+// insert the elements), and conversion will happen when the button is pressed.
+// Test data (pasted to textarea, including spaces):
+
+// Create element :
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+// Element variable :
+const textarea = document.querySelector("textarea");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  let trimmedArr = [];
+  const str = textarea.value;
+
+  const strNormalized = str.toLowerCase();
+  const strArr = strNormalized.split("\n");
+  for (const words of strArr) {
+    let [first, second] = words.split("_");
+
+    trimmedArr.push(
+      first.trim() + second.replace(second[0], second[0].toUpperCase())
+    );
+  }
+  for (const [i, word] of trimmedArr.entries()) {
+    console.log(`${word.padEnd(25, " ")}${"✅".repeat(i + 1)}`);
+  }
+});
+
+// underscore_case
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
+
+// Should produce this output (5 separate console.log outputs):
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+
+// Hints:
+// § Remember which character defines a new line in the textarea 😉
+// § The solution only needs to work for a variable made out of 2 words, like a_b
+// § Start without worrying about the ✅. Tackle that only after you have the variable
+// name conversion working 😉
+// § This challenge is difficult on purpose, so start watching the solution in case
+// you're stuck. Then pause and continue!
+
+// Afterwards, test with your own test data!
+
+// GOOD LUCK 😀
